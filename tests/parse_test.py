5271f62f60d287c7101c7ebbcab6562d29e22955
chrisism <cwjungerius@gmail.com>
2022-01-23 13:31:22 +0100

    test fixes

--- tests/parse_test.py
+++ tests/parse_test.py
@@ -1,29 +1,25 @@
-from re import sub
 import unittest, os
 import unittest.mock
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 
 import logging
 
+from tests.fakes import FakeFile
 from resources.lib.scraper import ScreenScraper
 
 logging.basicConfig(format = '%(asctime)s %(module)s %(levelname)s: %(message)s',
                 datefmt = '%m/%d/%Y %I:%M:%S %p', level = logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-def get_setting(key:str):
-    if key == 'scraper_cache_dir':
-         return os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),'output/'))
-    return 'test'
-
 def get_setting_int(key:str):
     return 0
 
 class ParseTest(unittest.TestCase):
-    
+
+    @patch('akl.scrapers.kodi.getAddonDir', autospec=True, return_value=FakeFile("/test"))
+    @patch('akl.scrapers.settings.getSettingAsFilePath', autospec=True, return_value=FakeFile("/test"))    
     @patch('resources.lib.scraper.settings.getSettingAsInt', autospec=True, side_effect=get_setting_int)
-    @patch('resources.lib.scraper.settings.getSetting', autospec=True, side_effect=get_setting)
-    def test_if_parsing_num_players_is_correct(self, setting_mock, setting_int_mock):
+    def test_if_parsing_num_players_is_correct(self, setting_int_mock, cachedir_mock, addondir_mock):
         # arrange
         target  = ScreenScraper()
         subject = { 'joueurs': { 'text': '1-4'}}
